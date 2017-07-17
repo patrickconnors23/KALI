a3bb5905cf8b10ce7ee897256241688fa57c5ca1
Patrick <patrickconnors@college.harvard.edu>
2017-07-17 13:07:12 -0400

    test

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-/*
- * Copyright 2016-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-
 /* jshint node: true, devel: true */
 'use strict';
 
@@ -774,9 +765,9 @@ function sendTypingOn(recipientId) {
 
 function initializeWindow() {
   var messageData = {
-    // recipient: {
-    //   id: recipientId
-    // },
+    recipient: {
+      id: recipientId
+    },
     message: {
       attachment: {
         type: "template",
