9bcafe33bc462de8147f4a5f3d0e285637713707
Patrick <patrickconnors@college.harvard.edu>
2017-07-17 11:12:51 -0400

    test

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(express.static('public'));
-
+console.log("hIT APP");
 /*
  * Be sure to setup your config values before running this code. You can
  * set them using environment variables or modifying the config file in /config.
