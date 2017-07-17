fff11c8ba6c095e62b62416db83ae4117fb6aea4
Patrick <patrickconnors@college.harvard.edu>
2017-07-17 13:23:18 -0400

    test

--- app.js
+++ app.js
@@ -364,9 +364,12 @@ function receivedPostback(event) {
   console.log("Received postback for user %d and page %d with payload '%s' " +
     "at %d", senderID, recipientID, payload, timeOfPostback);
 
-  // When a postback is called, we'll send a message back to the sender to
-  // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
+  switch (payload) {
+    case 'Get Started':
+      sendButtonMessage(senderID);
+      break;
+    default:
+      sendTextMessage(senderID, "Postback called");
 }
 
 /*
